dce3c28795c76aaddf04907a8ca69d80103fd461
Danyy41 <dany.mhaidly@gmail.com>
2026-06-06 11:22:55 -0400

    Create rules.py

---
+++ rules.py
@@ -0,0 +1,16 @@
+RULES = {
+    "SQL": {
+        "severity": "High",
+        "action": "Review application input validation"
+    },
+
+    "API key": {
+        "severity": "Critical",
+        "action": "Rotate key immediately"
+    },
+
+    "S3": {
+        "severity": "Medium",
+        "action": "Restrict bucket permissions"
+    }
+}
